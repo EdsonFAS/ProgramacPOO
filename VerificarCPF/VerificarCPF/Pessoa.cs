4e755054b237ffa3440015338827cadba9266ba2
EdsonFAS <111073194+EdsonFAS@users.noreply.github.com>
2023-08-30 20:38:55 -0400

    Add files via upload

---
+++ VerificarCPF/VerificarCPF/Pessoa.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerificarCPF
+{
+    public static class Pessoa
+    {
+        private static string cpf { get; set; }
+
+        
+        public static bool Validar(string cpf)
+        {
+            cpf = cpf.Replace(".","");
+            cpf = cpf.Replace("-","");
+            cpf = cpf.Trim();
+            int soma = 0, decr = 10, resto=0, result2=0, soma2=0;
+
+            
+            if (cpf.Length == 11)
+            {
+                for (int i = 0;i < 9; i++) {
+                    soma += decr *Convert.ToInt32(cpf[i].ToString());
+                   
+                   decr--;
+                }
+                if(soma%11==0 || soma%11==1) {
+
+                    if (0 == Convert.ToInt32(cpf[9].ToString())) {
+                        decr = 11;
+                        for (int i = 0; i < 10; i++)
+                        {
+                            soma2 += decr * Convert.ToInt32(cpf[i].ToString());
+
+                             decr--;
+                        }
+
+                        if (soma2%11==0 || soma2 % 11 == 1)
+                        { if (0 == Convert.ToInt32(cpf[10].ToString()))
+                            {
+                                return true;
+                            }
+                        }
+
+                      
+                        result2= soma2 % 11;
+
+                        result2 = 11 - result2;
+                        if (result2 == Convert.ToInt32(cpf[10].ToString()))
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                   
+
+                }
+               else
+                {
+                    resto= soma%11;
+
+                    resto = 11 - resto;
+                        if(resto== Convert.ToInt32(cpf[9].ToString()))
+                    {
+                        decr = 11;
+                        for (int i = 0; i < 10; i++)
+                        {
+                            soma2 += decr * Convert.ToInt32(cpf[i].ToString());
+
+                            decr--;
+                        }
+
+                        if (soma2 % 11 == 0 || soma2 % 11 == 1)
+                        {
+                            if (0 == Convert.ToInt32(cpf[10].ToString()))
+                            {
+                                return true;
+                            }
+                        }
+
+
+                        result2 = soma2 % 11;
+
+                        result2 = 11 - result2;
+                        if (result2 == Convert.ToInt32(cpf[10].ToString()))
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+
+
+
+
+                    }
+                    else
+                    {
+                        return false;
+                    }
+
+                }
+              
+
+            }
+              return false; 
+        }
+       
+
+
+        
+    }
+}
---
+++ VerificarCPF/VerificarCPF/Program.cs
@@ -0,0 +1,10 @@
+﻿using VerificarCPF;
+
+Console.WriteLine("-----Verificador de CPD-----");
+Console.WriteLine("Digite o CPF Que deseja verificar:");
+if (Pessoa.Validar("03892501203 "))
+{
+    Console.WriteLine("Valido");
+
+}
+Console.ReadKey();
