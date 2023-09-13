3fee07b4956f68a021bd1d41f35a3a381fe3dc6f
EdsonFAS <111073194+EdsonFAS@users.noreply.github.com>
2023-09-13 18:17:19 -0400

    Add files via upload

---
+++ Voo_passagens/Voo_passagens/Program.cs
@@ -0,0 +1,47 @@
+﻿
+using Voo_passagens;
+
+
+Console.WriteLine("digite a quantidade de passagens");
+
+int quant = 0;
+quant = Convert.ToInt32(Console.ReadLine());
+
+
+if (quant != 0)
+{
+    Voo n = new Voo(quant);
+
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine("Digite o numero da poltrona:");
+    int polt = Convert.ToInt32(Console.ReadLine());
+
+    n.ocupa(polt);
+
+}
+string[] mostrar= new string [n._acentos.Length];
+mostrar= n.mostrar(n._acentos);
+    int coluna1 = 0;
+    string m = "";
+for (int i = 0;i < mostrar.Length; i++)
+{
+        if (coluna1 <6)
+        {
+            m += $"{mostrar[i]}";
+            coluna1++;
+
+        }
+        else
+        {
+            m += $"\n";
+            coluna1 = 0;
+        }
+
+
+
+
+}
+Console.WriteLine(m);
+}
