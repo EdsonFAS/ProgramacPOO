a67ff8b37f1dbac759ef1156597afb08dc738b5d
EdsonFAS <111073194+EdsonFAS@users.noreply.github.com>
2023-09-14 21:58:19 -0400

    Add files via upload

---
+++ gabarito_prova/gabarito_prova/Gabarito.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gabarito_prova
+{
+    internal class Gabarito
+    {
+    public List<char> gabaritoList = new List<char>();
+
+
+        public Gabarito() {
+
+            gabaritoList.Add('A');
+            gabaritoList.Add('C');
+            gabaritoList.Add('E');
+            gabaritoList.Add('D');
+            gabaritoList.Add('B');
+
+        
+        
+        }
+    }
+}
---
+++ gabarito_prova/gabarito_prova/prova.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gabarito_prova
+{
+    internal class prova
+    {
+
+        public Gabarito Gabarit ;
+        public List<char> alternativas = new List<char>();
+        public int nota;
+        public int acerto;
+
+
+        public prova() {
+
+            Gabarit = new Gabarito();
+
+        }
+
+
+        public int acertos() {
+             acerto = 0;
+            for (int i = 0; i < alternativas.Count; i++)
+            {
+
+
+                if (alternativas[i] == Gabarit.gabaritoList[i])
+                { acerto++;             
+                
+                }
+
+            }
+
+            return acerto;
+        }
+
+
+        public int Nota(int acertos) {
+            nota = 0;
+            nota = acerto * 2;
+        
+        return nota;
+        
+        }
+        public void respostaAluno(List<char> alternat)
+        {
+
+
+            this.alternativas= alternat;
+
+        }
+        public static int maior(List<prova> provas)
+        {
+           
+            prova v = provas.OrderByDescending(x => x.nota).FirstOrDefault();
+
+            return v.nota ;
+        }
+
+    }
+}
